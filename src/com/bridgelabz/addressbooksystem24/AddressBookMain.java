8547c8dc6a9b78d119855eed7b73ef00c6d070e3
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-29 12:46:03 +0530

    Added:Welcome To Address Book System

---
+++ src/com/bridgelabz/addressbooksystem24/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbooksystem24;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Address Book System in Streams API");
+
+	}
+
+}
