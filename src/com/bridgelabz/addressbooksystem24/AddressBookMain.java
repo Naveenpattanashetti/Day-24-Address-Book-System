fa39266de46fd6b1410044799c2d29057956f06e
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-29 13:09:00 +0530

    Added:Add New Contact To Address Book Using Console

--- src/com/bridgelabz/addressbooksystem24/AddressBookMain.java
+++ src/com/bridgelabz/addressbooksystem24/AddressBookMain.java
@@ -1,11 +1,14 @@
 package com.bridgelabz.addressbooksystem24;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AddressBookMain {
-
 	public static void main(String[] args) {
-		System.out.println("Welcome to the Address Book System in Streams API");
+		System.out.println("Welcome into the Address Book System in Stream");
 		AddressBook addressBook = new AddressBook();
-
+		addressBook.operation();
 	}
 
 }
